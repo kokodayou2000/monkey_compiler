e79f5595cf9a050fb35b3cf00cb2ddc6744d836d
deng1611601667 <1611601667@qq.com>
2025-04-06 16:27:10 +0800

    定义 lexer 文件，实现简单是 lexer 功能

--- js/src/lexer.js
+++ js/src/lexer.js
@@ -27,36 +27,84 @@ export class Lexer {
 
     // 跳过输入中的空白字符
     skipWhitespace() {
-        // TODO: 实现逻辑以忽略空格、制表符和换行符
+        while(
+            this.ch === ' ' ||
+            this.ch === '\t' ||
+            this.ch === '\n' ||
+            this.ch === '\r'
+        ){
+            this.readChar()
+        }
     }
 
     // 读取下一个字符并更新当前字符
     readChar() {
-        // TODO: 实现逻辑以移动到输入中的下一个字符
+        // 如果到达输入字符串的末尾，则将当前字符设置为 undefined（表示 EOF）
+        // 否则，将当前字符设置为下一个字符
+        if(this.position >= this.input.length){
+            this.ch = undefined // EOF
+        }else{
+            this.ch = this.input[this.nextPosition]
+        }
+        this.position = this.nextPosition
+        this.nextPosition++
     }
 
     // 判断给定字符是否是字母（用于标识符）
     isLetter(ch) {
-        // TODO: 实现逻辑以检查字符是否为有效字母
+        if(ch === undefined) return false
+
+        // 使用正则表达式检查字符是否为字母或下划线
+        return /[a-z]/i.test(ch) || ch === '_'
     }
 
     // 判断给定字符是否是数字（用于数字标记）
     isDigit(ch) {
-        // TODO: 实现逻辑以检查字符是否为数字
+        if(ch === undefined) return false
+        // 使用 parseInt 检查字符是否为数字
+        // parseInt 返回 NaN 时表示不是数字
+        return !isNaN(parseInt(ch)) && ch != ''
+    }
+
+    // 读取一个标识符标记
+    readIdentifier(){
+        let start = this.position
+        // 循环读取字符，直到遇到非字母字符
+        while(this.isLetter(this.ch)){
+            this.readChar()
+        }
+        // 返回从开始位置到当前位置之间的子字符串作为标识符
+        return this.input.substring(start, this.position)
     }
 
     // 读取一个数字标记
     readNumber() {
-        // TODO: 实现逻辑以解析数字标记
+        let start = this.position
+        // 循环读取字符，直到遇到非数字字符
+        while(this.isDigit(this.ch)){
+            this.readChar()
+        }
+        // 返回从开始位置到当前位置之间的子字符串作为数字
+        return this.input.substring(start, this.position)
     }
 
     // 读取一个字符串标记
     readString() {
-        // TODO: 实现逻辑以解析字符串标记
+        let start = this.position + 1 // 跳过开头的引号
+        // 循环读取字符，直到遇到结束引号
+        while(this.ch !== '"' && this.ch !== undefined){
+            this.readChar()
+        }
+        // 返回从开始位置到当前位置之间的子字符串作为字符串
+        return this.input.substring(start, this.position)
     }
 
     // 查看下一个字符但不移动当前位置
     peekChar() {
-        // TODO: 实现逻辑以查看下一个字符
+        if(this.nextPosition >= this.input.length){
+            return undefined // EOF
+        }else{
+            return this.input[this.nextPosition]
+        }
     }
 }
